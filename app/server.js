b07cb6d81f7659bc6ea8ccd0693e23d70287a1d0
amacdexp <aron.macdonald01@sap.com>
2022-04-26 16:56:18 +0800

    WIP

--- app/server.js
+++ app/server.js
@@ -8,9 +8,9 @@
 import express     from "express";
 import xsenv       from "@sap/xsenv";
 import hana         from "@sap/hana-client";
-import stringifyObj from 'stringify-object'
+import stringifyObj from 'stringify-object';
 
-
+const crypto = await import('crypto') //for User Session Random
 
 
 const app = express();
@@ -45,16 +45,25 @@ const asyncCallDB = async (req, res) => {
   var HanaOptions = xsenv.cfServiceCredentials({  name: "hc_default_hdi" });
   var HanaOptionsTenant1 = xsenv.cfServiceCredentials({  name: "hc_tenant1_hdi" });
   var HanaOptionsTenant2 = xsenv.cfServiceCredentials({  name: "hc_tenant2_hdi" });
+
+  const user = 'USER_ ' + crypto.randomUUID();
+
   var asHanaOptions =
-{
-    host: HanaOptions.host,
-    port: HanaOptions.port,
-    uid: HanaOptions.user,        //default tenant user
-    pwd: HanaOptions.password,    //default tenant password
-    pooling: true,
-    maxPoolSize: 10,
-    connectionLifetime: 60
-}
+  {
+      host: HanaOptions.host,
+      port: HanaOptions.port,
+      uid: HanaOptions.user,        //default tenant user
+      pwd: HanaOptions.password,    //default tenant password
+      ca:  HanaOptions.certificate,
+      pooling: true,
+      maxPoolSize: 10,
+      connectionLifetime: 60,
+      'sessionVariable:APPLICATION' : 'HC-POOL-TEST-NODE',
+      //'sessionVariable:APPLICATIONUSER' : user,   //BAD... creates connections by user
+      'sessionVariable:CASE_SENSITIVE' : 'true'
+  }
+
+
 
   
   var startDate   = new Date();
@@ -64,7 +73,7 @@ const asyncCallDB = async (req, res) => {
   var sqlMainDate = new Date();
   var sqlC2Date   = new Date(); 
   var disconnDate = new Date(); //End Step
-  //var totalTime  = null;
+
   var timeDiff = null;
 
   var sql = '';
@@ -73,7 +82,7 @@ const asyncCallDB = async (req, res) => {
 
 
   var asConnection = hana.createClient(asHanaOptions);
-  //asConnection.setConnectionPoolLimit(10);   ??? Documentation
+  //hana.setConnectionPoolLimit(10);   ??? Documentation
   asConnection.connect(function(err) {
     //if (err) throw err;      //crash the app
     if (err)  {
@@ -86,6 +95,9 @@ const asyncCallDB = async (req, res) => {
       timeDiff = connDate - startDate ;
       outStr += startDate.toISOString()  + ' ' + connDate.toISOString() + ' ' + timeDiff  + ' ' ; 
 
+      //asConnection.exec(`SET 'CASE_SENSITIVE' = '${caseSensitivity}'`SET 'APPLICATIONUSER' = ');
+      //asConnection.exec(`SET 'APPLICATIONUSER' = '${applicationUser}'`);
+      //asConnection.exec(`SET 'APPLICATION' = 'DMS|${applicationSource}'`);
 
       //STEP1: Use CONNECT SQL with different user (e.g. tenant/HDI user) to better reuse connections
       if (req.query.tenant === '2' ) {
@@ -108,7 +120,7 @@ const asyncCallDB = async (req, res) => {
 
 
         //STEP2: set session variables for user
-        sql = "SET SESSION 'APPLICATION' = 'HC-APP';"
+        sql = "SET 'APPLICATIONUSER' = '" + user + "'";
         asConnection.exec(sql);
 
         sqlSetDate = new Date();
@@ -133,9 +145,9 @@ const asyncCallDB = async (req, res) => {
         timeDiff = sqlMainDate - sqlSetDate;
         outStr += sqlMainDate.toISOString()  + "\t" + timeDiff  + "\t" ; 
 
-        //STEP4: Connect back with original DEFAULT user to restore connection for reuse
+        //STEP4 [OPTIONAL]: Connect back with original DEFAULT user to restore connection 
         sql =  ' CONNECT ' + asHanaOptions.uid + ' password "' + asHanaOptions.pwd + '";';
-        asConnection.exec(sql);
+        //asConnection.exec(sql);
 
         sqlC2Date = new Date();
         timeDiff = sqlC2Date - sqlMainDate;
@@ -192,39 +204,47 @@ const asyncCallDBstd = async (req, res) => {
   var HanaOptions = xsenv.cfServiceCredentials({  name: "hc_default_hdi" });
   var HanaOptionsTenant1 = xsenv.cfServiceCredentials({  name: "hc_tenant1_hdi" });
   var HanaOptionsTenant2 = xsenv.cfServiceCredentials({  name: "hc_tenant2_hdi" });
+
+  const user = 'USER_ ' + crypto.randomUUID();
+
+ 
   var asHanaOptions =
-{
-    host: HanaOptions.host,
-    port: HanaOptions.port,
-    uid: HanaOptions.user,        //default tenant user
-    pwd: HanaOptions.password,    //default tenant password
-    pooling: true,
-    maxPoolSize: 5,               //  reduced 10 to 5 to better compare performance  between both solutions
-    connectionLifetime: 60
-}
+  {
+      host: HanaOptions.host,
+      port: HanaOptions.port,
+      uid: HanaOptions.user,        //default tenant user
+      pwd: HanaOptions.password,    //default tenant password
+      ca:  HanaOptions.certificate,
+      pooling: true,
+      maxPoolSize: 5,               //  reduced 10 to 5 to better compare performance  between both solutions
+      connectionLifetime: 60,
+      'sessionVariable:APPLICATION' : 'HC-POOL-TEST-NODE',
+      //'sessionVariable:APPLICATIONUSER' : user,   //BAD... creates connections by user
+      'sessionVariable:CASE_SENSITIVE' : 'true',
+  }
 
 
-if (req.query.tenant === '2' ) {
-  //Customer Tenant 2
-  asHanaOptions.uid =   HanaOptionsTenant2.user;
-  asHanaOptions.pwd =   HanaOptionsTenant2.password;
+  if (req.query.tenant === '2' ) {
+    //Customer Tenant 2
+    asHanaOptions.uid =   HanaOptionsTenant2.user;
+    asHanaOptions.pwd =   HanaOptionsTenant2.password;
 
-} else  {
-  //Customer Tenant 1
-  asHanaOptions.uid =   HanaOptionsTenant1.user;
-  asHanaOptions.pwd =   HanaOptionsTenant1.password;
-}
+  } else  {
+    //Customer Tenant 1
+    asHanaOptions.uid =   HanaOptionsTenant1.user;
+    asHanaOptions.pwd =   HanaOptionsTenant1.password;
+  }
 
 
   
   var startDate   = new Date();
   var connDate    = new Date();
-  var sqlC1Date   = new Date();
+  //var sqlC1Date   = new Date();
   var sqlSetDate  = new Date();
   var sqlMainDate = new Date();
-  var sqlC2Date   = new Date(); 
+  //var sqlC2Date   = new Date(); 
   var disconnDate = new Date(); //End Step
-  //var totalTime  = null;
+
   var timeDiff = null;
 
   var sql = '';
@@ -254,21 +274,21 @@ if (req.query.tenant === '2' ) {
       timeDiff = connDate - startDate ;
       outStr += startDate.toISOString()  + ' ' + connDate.toISOString() + ' ' + timeDiff  + ' ' ; 
 
+      //STEP2: set session variables for user
+      sql = "SET 'APPLICATIONUSER' = '" + user + "'";
+      asConnection.exec(sql);
 
-      //STEP1: Set Session variables
-      sql = "SET SESSION 'APPLICATION' = 'HC-APP';"
-      asConnection.exec(sql, function (err, results) {
+      sqlSetDate = new Date();
+      timeDiff = sqlSetDate -  connDate;
+      outStr += sqlSetDate.toISOString()  + "\t" + timeDiff  + "\t" ; 
+
+
+      sql = 'SELECT CURRENT_USER ,CURRENT_TIMESTAMP, CURRENT_SCHEMA, ack(1,0) from DUMMY;' // slow down with recursive a function
+      asConnection.exec(sql, function (err, rs) {
       if (err) throw err;
 
-        sqlSetDate = new Date();
-        timeDiff = sqlSetDate - connDate;
-        outStr += sqlSetDate.toISOString()  + "\t" + timeDiff  + "\t" ; 
+        
 
-
-        //STEP2: Excute MAIN SQL
-        //sql = "SELECT CURRENT_TIMESTAMP FROM DUMMY;"  //  Very fast
-        sql = 'SELECT CURRENT_USER ,CURRENT_TIMESTAMP, CURRENT_SCHEMA, ack(1,0) from DUMMY;' // slow down with recursive a function
-        var rs = asConnection.exec(sql);
         var rsUser = ''
         try {
           rsUser = rs[0].CURRENT_USER
@@ -276,7 +296,6 @@ if (req.query.tenant === '2' ) {
         } catch(err) {
           rsUser = 'ERROR'
         }  
-
 
         sqlMainDate = new Date();
         timeDiff = sqlMainDate - sqlSetDate;
@@ -286,7 +305,7 @@ if (req.query.tenant === '2' ) {
         //FINAL STEP:    Disconnect - return to pool (if applicable)
         asConnection.disconnect();
         disconnDate = new Date();
-        timeDiff = disconnDate - sqlC2Date;
+        timeDiff = disconnDate - sqlMainDate;
         outStr += disconnDate.toISOString()  + "\t" + timeDiff  + "\t" ; 
   
         //Finish including Total time spent
